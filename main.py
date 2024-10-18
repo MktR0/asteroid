586f97755d68d65d381d29bc73f60d898c99cb0f
MktR0 <mktronr@gmail.com>
2024-10-17 20:26:52 -0400

    draw player to screen

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     running = True
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
