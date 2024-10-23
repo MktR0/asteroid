eeec7ccc2c39adf28e7077661c27c5d0b5738b93
MktR0 <mktronr@gmail.com>
2024-10-22 23:08:56 -0400

    added update and draw groups added asteroid added asteroidfield

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,21 +13,41 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    running = True
+    
+    drawable_group = pygame.sprite.Group()
+    updatable_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+    asteroid_field_group = pygame.sprite.Group()
+
+
+
+    Asteroid.containers = (asteroid_group, updatable_group, drawable_group)
+    AsteroidField.containers = updatable_group
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for updatable_obj in updatable_group:
+            updatable_obj.update(dt)
+
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        # drawable_group.draw(screen) 
+        for drawable_obj in drawable_group:
+            drawable_obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
